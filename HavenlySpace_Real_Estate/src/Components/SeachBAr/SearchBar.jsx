cfb87667d8439f46ec375ebe55a970cfa4b8d860
uvindux <uvindumanohara@icloud.com>
2025-11-26 11:03:07 +0530

    Enhances search bar with form and UI controls
    Introduces interactive search options, including buy/rent toggle and filters for location and price range.
    Improves usability and prepares for search functionality integration.

--- HavenlySpace_Real_Estate/src/Components/SeachBAr/SearchBar.jsx
+++ HavenlySpace_Real_Estate/src/Components/SeachBAr/SearchBar.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function SearchBar() {
   return (
-    <div>SearchBar</div>
+    <div>
+          <div className="type">
+                    <button>Buy</button>
+                    <button>Rent</button>
+          </div>
+
+          <form action="">
+                    <input type="text" name='location' placeholder='CityLocation' />
+                    <input type="number " name='MinPrice' min={0} max={10000} placeholder='Min Price' />
+                    <input type="number" name='MaxPrice' min={0} max={10000} placeholder='Max Price' />
+                    <button><img src="/search.png" alt="" srcset="" /></button>
+          </form>
+    </div>
   )
 }
 
