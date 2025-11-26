e8301e2c04533f24b38b7c13a66d00ac926a02f1
uvindux <uvindumanohara@icloud.com>
2025-11-26 10:51:51 +0530

    Enhances homepage with search bar and stats section
    Improves homepage user experience by adding a search bar and a new section displaying company statistics, highlighting experience, awards, and property count. Updates layout and styling for better visual hierarchy and engagement.

--- HavenlySpace_Real_Estate/src/routes/Homepage/Homepage.jsx
+++ HavenlySpace_Real_Estate/src/routes/Homepage/Homepage.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import './Homepage.scss'
+import SearchBar from '../../Components/SeachBAr/SearchBar.jsx'
+
 
 function Homepage() {
   return (
     <div className="HomePage">
-          <div className="textContainer">
-                    <div className="Wrapper">
-                              <h1>Find Real Estate & Get Your Dream Place </h1>
-                              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius accusamus reiciendis quae, nobis nulla sed enim incidunt quam veniam rem! Non, accusamus obcaecati dolor odio atque debitis cum impedit assumenda.</p>
-                              </div>
-                              
-                              </div>
+      <div className="textContainer">
+        <div className="Wrapper">
+          <h1 className='MainHeader'>Find Real Estate & Get Your Dream Place </h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius accusamus reiciendis quae, nobis nulla sed enim incidunt quam veniam rem! Non, accusamus obcaecati dolor odio atque debitis cum impedit assumenda.</p>
+          <SearchBar />
+          <div className="Boxes">
+            <div className="Box">
+            <h1 className='Number'>16+</h1>
+            <h1 className='Description'>Years Of Experience</h1>
+          </div>
+          <div className="Box">
+            <h1 className='Number'>200</h1>
+            <h1 className='Description'>Awards Gained</h1>
+          </div>
+          <div className="Box">
+            <h1 className='Number'>2000+</h1>
+            <h1 className="Description">Property Ready</h1>
+          </div>
+          </div>
+          
+        </div>
 
-          <div className="imageContainer"><img src="/bg.png" alt="" srcset="" /></div>
+
+
+
+      </div>
+
+      <div className="imageContainer"><img src="/bg.png" alt="" srcset="" /></div>
+
     </div>
   )
 }
