4cca2f1694b6b8fd59a537d5434f6ee06cb9a4ed
uvindux <uvindumanohara@icloud.com>
2025-11-28 16:34:02 +0530

    Adds filter UI component to listings page
    Introduces a filter section to enhance user ability to refine property searches.
    Lays groundwork for improved map integration and future feature expansion.

--- HavenlySpace_Real_Estate/src/routes/list/ListPage.jsx
+++ HavenlySpace_Real_Estate/src/routes/list/ListPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Listdata from '../../lib/DummyData'
+import Filter from '../../Components/filter/Filter';
 
 function ListPage() {
           const Data = Listdata;
   return (
-    <div>
+            <div>
+                      <div className="filterComponent">
+                                <div className="wrapper">
+                                          <Filter/>
+                                          
+                                </div>
+                      </div>
+                      <div className="mapComponent"></div>
       
     </div>
   )
